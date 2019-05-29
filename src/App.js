e465051a9708bb4a5f5ccbddabfc1c2a94d4a604
dalmano <dalmano@outlook.com>
2019-05-29 17:00:48 +0100

    feature/letters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import UserHeader from "./components/UserHeader";
+import Letters from "./components/Letters";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Ryan is lovely</h1>
       <UserHeader />
+      <Letters />
     </div>
   );
 }
